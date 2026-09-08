3a59bd7d86db597aeee5a7107f07a8a4d8c649d5
MrMee0007 <meezana033@gmail.com>
2026-09-08 10:58:29 +0530

    debug: add request handler error trap in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,2 +1,12 @@
-const app = require('../backend/server.js')
-module.exports = app
+module.exports = (req, res) => {
+  try {
+    const app = require('../backend/server.js')
+    return app(req, res)
+  } catch (err) {
+    res.status(500).json({
+      error: 'Serverless Boot Error',
+      message: err.message,
+      stack: err.stack
+    })
+  }
+}
