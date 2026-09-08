d4cc901b0bed8c61cf4bd83f791855c5f8f2c0e9
MrMee0007 <meezana033@gmail.com>
2026-09-08 11:00:19 +0530

    fix: remove type module from package.json so Node executes CommonJS require in serverless function

--- api/index.js
+++ api/index.js
@@ -1,12 +1,2 @@
-module.exports = (req, res) => {
-  try {
-    const app = require('../backend/server.js')
-    return app(req, res)
-  } catch (err) {
-    res.status(500).json({
-      error: 'Serverless Boot Error',
-      message: err.message,
-      stack: err.stack
-    })
-  }
-}
+const app = require('../backend/server.js')
+module.exports = app
